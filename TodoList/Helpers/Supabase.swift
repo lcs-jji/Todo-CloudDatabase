271908d19dd5c3a028dfad53816394f61c88e668
Joseph Ji <jji@lcs.on.ca>
2025-04-03 10:04:29 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Xinchen Ji on 2025-04-03.
+//
+
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://foplvqytdfllkvsywokj.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvcGx2cXl0ZGZsbGt2c3l3b2tqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2ODc0MTMsImV4cCI6MjA1OTI2MzQxM30.CwY2mCbZ5mGeD-LaiGk-JZwNYpP6exoUSioyQlbLpY0"
+)
+ 
